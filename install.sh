8c917eed405fbb59e08d4ea5bade183ef3c210a1
dan-adsbx <129466232+dan-adsbx@users.noreply.github.com>
2024-04-04 16:12:54 -0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@ ephem_auth_key="tskey-auth-kpCKaLg84v11CNTRL-YFaEuPqS5eWPZzxa4m43kW93ppwPwK35"
 
 curl -fsSL https://tailscale.com/install.sh | sh
 read -r line < /boot/adsbx-uuid; serialno=${line:0:8}
-tailscale up --hostname=receiver-$serialno --authkey=$ephem_auth_key
+tailscale up --ssh --hostname=receiver-$serialno --authkey=$ephem_auth_key
 
 
 
