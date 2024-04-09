ea8021bee1ef6f42127b7be38246f60bc786a779
dan-adsbx <129466232+dan-adsbx@users.noreply.github.com>
2024-04-09 09:02:06 -0700

    Update install.sh
    Change to non-ephemeral

--- install.sh
+++ install.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-ephem_auth_key="tskey-auth-kJjGTi6uNL11CNTRL-XpaxYZj2wkQtKtGCKZvnkQ9Jzjiu8r5UM"
+feeder_auth_key="tskey-auth-kq7EBS6R9b11CNTRL-ydTE4VdXNiMztLGwkHZYiMqKMnGbCNbvV"
 read -r line < /boot/adsbx-uuid; serialno=${line:0:8}
 
 curl -fsSL https://tailscale.com/install.sh | sh
 
-tailscale up --authkey=$ephem_auth_key --hostname=receiver-$serialno --ssh --accept-routes
+tailscale up --authkey=$feeder_auth_key --hostname=receiver-$serialno --ssh --accept-routes
 
 
 
