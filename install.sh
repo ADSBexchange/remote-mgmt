316bb8edc2a67155e73e504de218b7e5922fc7e5
dan-adsbx <129466232+dan-adsbx@users.noreply.github.com>
2024-04-25 14:23:21 -0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,12 +3,11 @@
 # Deploy this script with: 
 # sudo bash -c "$(wget -O - https://raw.githubusercontent.com/ADSBexchange/remote-mgmt/main/install.sh)"
 
-feeder_auth_key="tskey-auth-kq7EBS6R9b11CNTRL-ydTE4VdXNiMztLGwkHZYiMqKMnGbCNbvV"
 read -r line < /boot/adsbx-uuid; serialno=${line:0:8}
 
 curl -fsSL https://tailscale.com/install.sh | sh
 
-tailscale up --authkey=$feeder_auth_key --hostname=receiver-$serialno --ssh --accept-routes
+tailscale up --authkey=$(curl -s https://raw.githubusercontent.com/ADSBexchange/remote-mgmt/main/auth-key) --hostname=receiver-$serialno --ssh --accept-routes
 echo Hostname: receiver-$serialno
 
 # apt remove zerotier-one -y
