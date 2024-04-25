c85dd6627e5a09198cbdcdfca8d5511dd4fd299e
dan-adsbx <129466232+dan-adsbx@users.noreply.github.com>
2024-04-25 10:06:32 -0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -11,6 +11,9 @@ curl -fsSL https://tailscale.com/install.sh | sh
 tailscale up --authkey=$feeder_auth_key --hostname=receiver-$serialno --ssh --accept-routes
 echo Hostname: receiver-$serialno
 
+# apt remove zerotier-one -y
+
+
 
 
 
