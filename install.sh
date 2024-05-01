dd9be57192d4b2dfd5ec57a5b6e9f683cc6933d9
dan-adsbx <129466232+dan-adsbx@users.noreply.github.com>
2024-05-01 14:37:14 -0700

    Update install.sh
    Add swapfile stuff.

--- install.sh
+++ install.sh
@@ -5,7 +5,10 @@
 
 read -r line < /boot/adsbx-uuid; serialno=${line:0:8}
 
+# Swapfile needed to run apt update on low RAM Pis.
+dphys-swapfile swapon
 curl -fsSL https://tailscale.com/install.sh | sh
+dphys-swapfile swapoff
 
 tailscale up --authkey=$(curl -s https://raw.githubusercontent.com/ADSBexchange/remote-mgmt/main/auth-key) --hostname=receiver-$serialno --ssh --accept-routes
 echo Hostname: receiver-$serialno
